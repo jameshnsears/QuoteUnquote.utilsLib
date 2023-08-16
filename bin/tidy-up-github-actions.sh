5766f5b1e4315c6b480de72675974122492e7064
James HN Sears <james.hn.sears@gmail.com>
2023-08-16 13:25:26 +0100

    support for 4.35.0

--- bin/tidy-up-github-actions.sh
+++ bin/tidy-up-github-actions.sh
@@ -3,14 +3,16 @@
 source envvars.sh
 
 if [ $# -ne 2 ]; then
-  echo "Usage: $0 <BRANCH> <NUMBER_OF_RUNS>"
+  echo "Usage: $0 <BRANCH_WILDCARD> <NUMBER_OF_RUNS>"
   exit 1
 fi
 
-BRANCH=$1
+BRANCH=$1         # can be "specific" wildcard
 NUMBER_OF_RUNS=$2
 
-gh run list -b "${BRANCH}" --json databaseId  -q '.[].databaseId' -L ${NUMBER_OF_RUNS} |
-  xargs -IID gh api \
-    "repos/$(gh repo view --json nameWithOwner -q .nameWithOwner)/actions/runs/ID" \
-    -X DELETE
+gh run list \
+  --json headBranch,databaseId --jq '.[] | select(.headBranch | test("dependabot/")) | .databaseId' \
+  -L 100 | \
+xargs -IID gh api \
+  "repos/$(gh repo view --json nameWithOwner -q .nameWithOwner)/actions/runs/ID" \
+  -X DELETE
