5f7e5c7e07d3316e2eb8c86921b64040605c8bed
James HN Sears <james.hn.sears@gmail.com>
2025-07-11 13:35:40 +0300

    Update Gradle configuration; upgrade dependencies and improve Kotlin support

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,11 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.10.0")
+        classpath("com.android.tools.build:gradle:8.13.0")
         classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.8")
         classpath("org.jacoco:org.jacoco.core:0.8.13")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
-        classpath("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:6.1.0.5360")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.20")
+        classpath("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:6.3.1.5724")
     }
 }
 
