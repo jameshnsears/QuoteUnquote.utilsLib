e966942f53d48e8e0d08df23976f73bad83d566b
James HN Sears <james.hn.sears@gmail.com>
2026-03-27 14:48:13 +0200

    "improve" github action

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,10 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.13.0")
+        classpath("com.android.tools.build:gradle:9.1.0")
         classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.8")
-        classpath("org.jacoco:org.jacoco.core:0.8.13")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.20")
+        classpath("org.jacoco:org.jacoco.core:0.8.14")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.20")
         classpath("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:6.3.1.5724")
     }
 }
